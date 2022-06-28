945fe5d19c06b5c99416165bf724c9a6f01a28c6
tinobie03 <tinotendachatora7@gmail>
2022-06-28 09:40:46 +0200

    Add this

---
+++ src/components/Tables/SimpleTable.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <md-table v-model="users" :table-header-color="tableHeaderColor">
+      <md-table-row slot="md-table-row" slot-scope="{ item }">
+        <md-table-cell md-label="Author-name">{{ item.author_name }}</md-table-cell>
+        <md-table-cell md-label="Research-name">{{ item.researchName }}</md-table-cell>
+        <md-table-cell md-label="Institute">{{ item.institute }}</md-table-cell>
+        <md-table-cell md-label="Details"><md-button>view</md-button></md-table-cell>
+      </md-table-row>
+    </md-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "simple-table",
+  props: {
+    tableHeaderColor: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      selected: [],
+      users: [
+        {
+          author_name: "Mr Rice",
+          researchName: "Transport",
+          institute: "CUT",
+          details: ""
+        },
+        {
+          author_name: "Mrs Hooper",
+          researchName: "Water",
+          institute: "Curaçao",
+          details: ""
+        },
+        {
+          author_name: "Sage Rodriguez",
+          researchName: "KIJ",
+          institute: "Netherlands",
+          details: ""
+        },
+        {
+          author_name: "Philip Chaney",
+          researchName: "CDF",
+          institute: "Korea, South",
+          details: ""
+        },
+        {
+          author_name: "Doris Greene",
+          researchName: "ABC",
+          institute: "Malawi",
+          details: ""
+        },
+        {
+          author_name: "Mason Porter",
+          researchName: "XYZ",
+          institute: "Chile",
+          details: ""
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ src/pages/UserProfile/EditProfileForm.vue
@@ -0,0 +1,110 @@
+<template>
+  <form>
+    <md-card>
+      <md-card-header :data-background-color="dataBackgroundColor">
+        <h4 class="title">Edit Profile</h4>
+        <p class="category">Complete your profile</p>
+      </md-card-header>
+
+      <md-card-content>
+        <div class="md-layout">
+          <!--<div class="md-layout-item md-small-size-100 md-size-33">
+            <md-field>
+              <label>School </label>
+              <md-input v-model="companyname" type="text"></md-input>
+            </md-field>
+          </div>-->
+         <!-- <div class="md-layout-item md-small-size-100 md-size-33">
+            <md-field>
+              <label>User Name</label>
+              <md-input v-model="username" type="text"></md-input>
+            </md-field>
+          </div>-->
+          <!--<div class="md-layout-item md-small-size-100 md-size-33">
+            <md-field>
+              <label>Email Address</label>
+              <md-input v-model="emailadress" type="email"></md-input>
+            </md-field>
+          </div>-->
+          <div class="md-layout-item md-small-size-100 md-size-33">
+            <md-field>
+              <label>Author First-Name</label>
+              <md-input v-model="firstname" type="text"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-33">
+            <md-field>
+              <label> AuthorLast-Name</label>
+              <md-input v-model="lastname" type="text"></md-input>
+            </md-field>
+          </div>
+           <div class="md-layout-item md-small-size-100 md-size-33">
+            <md-field>
+              <label>Email Address</label>
+              <md-input v-model="address" type="text"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-50">
+            <md-field>
+              <label>School/Faculty</label>
+              <md-input v-model="school" type="text"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-50">
+            <md-field>
+              <label>Department</label>
+              <md-input v-model="city" type="text"></md-input>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-33">
+            <md-field>
+              <label>Research Title</label>
+              <md-input v-model="country" type="text"></md-input>
+            </md-field>
+          </div>
+          <!--<div class="md-layout-item md-small-size-100 md-size-33">
+            <md-field>
+              <label>Postal Code</label>
+              <md-input v-model="code" type="number"></md-input>
+            </md-field>
+          </div>-->
+          <div class="md-layout-item md-size-100">
+            <md-field maxlength="5">
+              <label>Type your Resaarch</label>
+              <md-textarea v-model="Research"></md-textarea>
+            </md-field>
+          </div>
+          <div class="md-layout-item md-size-100 text-right">
+            <md-button class="md-raised md-success">Update Profile</md-button>
+          </div>
+        </div>
+      </md-card-content>
+    </md-card>
+  </form>
+</template>
+<script>
+export default {
+  name: "edit-profile-form",
+  props: {
+    dataBackgroundColor: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      username: null,
+      disabled: null,
+      emailadress: null,
+      lastname: null,
+      firstname: null,
+      address: null,
+      city: null,
+      country: null,
+      code: null,
+      
+    };
+  }
+};
+</script>
+<style></style>
